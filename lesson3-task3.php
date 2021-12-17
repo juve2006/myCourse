b9e56c64fe6334404c30956cffb6cf850831f442
Evgen Sikora <arnika200@gmail.com>
2021-12-17 14:25:10 +0200

    l3 t3 done

--- lesson3-task3.php
+++ lesson3-task3.php
@@ -13,21 +13,25 @@
 </form>
 
 <?php
-if (!empty($_POST['height'])) {
-    $h = $_POST['height'];
-    $l=$h+1;// довжина строки
-    for ($i=0; $i<$h; $i++){
-        for ($j=0;$j<$l-$i-2;$j++){
-            echo ' ';
+echo "<div>";
+    if (!empty($_POST['height'])) {
+        $h = $_POST['height'];
+    }
+if ($h>=1 && $h <=15) {
+    $l = $h + 1;// довжина строки
+    for ($i = 0; $i < $h; $i++) {
+        for ($j = 0; $j < $l - $i - 2; $j++) {
+            print ("&nbsp;&nbsp;");
         }
-        for ($k=0;$k<$i+2;$k++) {
-            echo "#";
+        for ($k = 0; $k < $i + 2; $k++) {
+            print ("#");
         }
         echo "<br>";
     }
+} else {
+    echo "Введіть висоту в діапазоні з 1 до 15";
 }
-//http://www.javaproblems.com/2012/11/java-problem-g-triangles-shape.html
-//https://question-it.com/questions/1148203/programmirovanie-na-c-cs50-pechat-blokov-piramidy-mario-dlja-trudnostej-petel
+echo "</div>";
 ?>
 </body>
 </html>
