58ee06802281c1d1c804982f31f507a87ee0e7ef
Evgen Sikora <arnika200@gmail.com>
2022-01-09 18:39:53 +0200

    l5 t3 done, t4 in progress

--- lesson5-task3.php
+++ lesson5-task3.php
@@ -17,50 +17,63 @@
 //2) Середнє значення (float);
 //3) Кількість парних чисел;
 //4) Всі непарні числа;
-	if (!empty($_POST['numbers'])) { //1, 2, 3, 4, 5, 6, 7, 8344, 55, -4, 53, 44, 40
+	if (!empty($_POST['numbers'])) { //1, 2, 3, 4, 5, 6, 7, 8344, 55, -4, 53, -43, 44, 40, 48, -7
         $numbers = $_POST['numbers'];
-		$arr = array_map('trim', explode(',', $numbers));
-        /*$arr = array_map('oInt', $arr);
-
-        foreach ($arr as $item){
-            if (!is_int($item)){
-                echo "Ви ввели не цілі числа, будь-ласка перевірти введені дані";
-                break;
-            }
+		if (preg_match('/^[0-9\-, ]+$/', $numbers )) {
+			$arr = array_map('trim', explode(',', $numbers));
+			//var_dump($arr);
+			function sum($array)
+			{
+				$sum = 0;
+				foreach ($array as $item) {
+					$sum += $item;
+				}
+				echo 'Сума введених чисел - ' . $sum;
+			}
+			
+			function avg($array)
+			{
+				$sum = 0;
+				foreach ($array as $item) {
+					$sum += $item;
+					$avg = round($sum / count($array), 2);
+				}
+				echo 'Середнє значення введених чисел- ';
+				printf('%0.2f', $avg);
+			}
+			
+			function evenCount($array)
+			{
+				foreach ($array as $key => $item) {
+					if ($item % 2 !== 0) {
+						unset($array[$key]);
+					}
+				}
+				echo 'Кількість парних чисел - ' . count($array);
+			}
+			
+			function odd($array)
+			{
+				foreach ($array as $key => $item) {
+					if ($item % 2 === 0) {
+						unset($array[$key]);
+					}
+				}
+				echo 'Непарні числа: ' . implode(',', $array);
+			}
+			
+			sum($arr);
+			echo '<br>';
+			avg($arr);
+			echo '<br>';
+			evenCount($arr);
+			echo '<br>';
+			odd($arr);
+		}
+		else {
+		    echo 'Ви ввели не тільки цілі числа';
         }
-       var_dump($arr);
-        function oInt($array){
-            foreach ($array as $ele){
-                $ele*=1;
-            }
-        }*/
-        function sum($array){
-            foreach ($array as $item) {
-                $sum+=$item;
-            }
-            echo 'Сума введених чисел - '.$sum;
-        }
-        function avg($array){
-            foreach ($array as $item) {
-                $sum+=$item;
-                $avg = round($sum/count($array), 2);
-            }
-            echo 'Середнє значення введених чисел - '.$avg;
-        }
-        function even($array){
-            foreach ($array as $key=>$item){
-                if ($item%2!==0) {
-                    unset($array[$key]);
-                }
-            }
-            echo 'Кількість парних чисел - '.count($array);
-        }
-        sum($arr);
-        echo '<br>';
-        avg($arr);
-        echo '<br>';
-        even($arr);
-    }
+	}
 ?>
 </body>
 </html>
